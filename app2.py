ec4f54ce924157d164b3ce48c2cbae8dc3f8a7d1
Wallo03 <walid.birdaha03@gmail.com>
2023-03-24 22:37:11 +0100

    update

--- app2.py
+++ app2.py
@@ -9,23 +9,27 @@ import seaborn as sns
 st.title("Toronto Covid Infections :chart_with_upwards_trend:")
 
 
-df = pd.DataFrame(
-    np.random.randn(20, 2) / [50, 15] + [43.68, -79.4],
-    columns=['lat', 'lon'])
+df = pd.DataFrame( np.random.randn(20, 2) / [50, 15] + [43.68, -79.4], columns=['lat', 'lon'])
 
 st.map(df)
 
-st.markdown('The Covid Cases in Toronto is controlled, and today we have a low range of new cases, so we are in a good position')
+st.markdown('The Covid Cases in Toronto is controlled, and today we have a low range of new cases, amd a high range of resolved cases, so we are in a good position')
+st.title("Toronto Covid Chart :chart_with_upwards_trend:")
 
-df = pd.read_csv('Dati.csv')
-df = df[ (df['PHU_NAME'] == 'TORONTO')]
+####################################################################
 
-lista = ['ACTIVE_CASES', 'RESOLVED_CASES', 'DEATHS']
-fig = plt.figure(figsize=(18,10))
-
-# ax = plt.gca()
+#fig = plt.figure(figsize=(18,10))
+# ax = plt.gca()  ----> NON SI USA QUESTO SU STREAMLIT
 # #for i in lista:
 # for i in lista:
-sns.countplot(y='ACTIVE_CASES', data=df)
+#sns.countplot(y='ACTIVE_CASES', data=df)
+#st.pyplot(fig)
 
-st.pyplot(fig)
+####################################################################
+
+df = pd.read_csv('csvFolder/Dati.csv')
+df = df[ (df['PHU_NAME'] == 'TORONTO')]
+df1 = pd.DataFrame(df,
+    columns=['ACTIVE_CASES', 'RESOLVED_CASES', 'DEATHS'])
+
+st.line_chart(df1)
