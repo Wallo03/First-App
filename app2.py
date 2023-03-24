e7fb440b6b55fea581e7abd4be6199c71ffe447f
Wallo <wallo@gmail.com>
2023-03-24 13:04:46 +0100

    sus

--- app2.py
+++ app2.py
@@ -17,15 +17,15 @@ st.map(df)
 
 st.markdown('The Covid Cases in Toronto is controlled, and today we have a low range of new cases, so we are in a good position')
 
-df = sns.load_dataset('Dati.csv')
+df = pd.read_csv('Dati.csv')
 df = df[ (df['PHU_NAME'] == 'TORONTO')]
 
 lista = ['ACTIVE_CASES', 'RESOLVED_CASES', 'DEATHS']
 fig = plt.figure(figsize=(18,10))
 
-ax = plt.gca()
-#for i in lista:
-for i in lista:
-    sns.countplot(kind='line', x='FILE_DATE', y=i, ax=ax)
+# ax = plt.gca()
+# #for i in lista:
+# for i in lista:
+sns.countplot(y='ACTIVE_CASES', data=df)
 
 st.pyplot(fig)
